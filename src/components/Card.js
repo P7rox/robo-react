7ad20b33672e0df63e06e62a77392284e8092eb4
Prajwal Goswami <prajwalgoswami1996@gmail.com>
2019-01-08 03:04:02 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const card = ({ name, email, id }) => {
   return (
     <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-      <img src={`https://robohash.org/${id}?200x200`} alt='pic' />
+      <img src={`https://robohash.org/${id}?size=200x200`} alt='pic' />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
