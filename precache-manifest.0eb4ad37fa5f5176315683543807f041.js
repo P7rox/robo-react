ada77067e0601ad7987ab1ddb19439b66e2c6b48
Prajwal Goswami <prajwalgoswami1996@gmail.com>
2019-01-08 08:41:23 +0530

    Updates

---
+++ precache-manifest.0eb4ad37fa5f5176315683543807f041.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-react/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "a503e9b9cb4043d883fb",
+    "url": "/robo-react/static/js/runtime~main.a503e9b9.js"
+  },
+  {
+    "revision": "f03db12537b3087e3781",
+    "url": "/robo-react/static/js/main.f03db125.chunk.js"
+  },
+  {
+    "revision": "21a3d3a101d8bc4e620e",
+    "url": "/robo-react/static/js/1.21a3d3a1.chunk.js"
+  },
+  {
+    "revision": "f03db12537b3087e3781",
+    "url": "/robo-react/static/css/main.8bd19be9.chunk.css"
+  },
+  {
+    "revision": "21a3d3a101d8bc4e620e",
+    "url": "/robo-react/static/css/1.77d08025.chunk.css"
+  },
+  {
+    "revision": "86248ad69027de9ee5bbae4198cd9fbe",
+    "url": "/robo-react/index.html"
+  }
+];
